5ebb9f32725081da14ec6db77f99a80edd8e7db2
EmmanuelAdu <christorenzo20@gmail.com>
2024-05-08 17:19:47 +0000

    Last In First Out

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Create a class LIFOCache that inherits from
+BaseCaching and is a caching system:
+
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Last In First Out Cache
+    """
+
+    def __init__(self):
+        """init
+        """
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """
+        Adds item to cache.
+        If the cache is full, the least recently used item is discarded.
+
+        :param key: key to store the item under
+        :param item: item to store
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                # if the cache is full and the key is not already in the cache,
+                # discard the least recently used item
+                last_key = next(reversed(self.cache_data))
+                del self.cache_data[last_key]
+                print("DISCARD: {}".format(last_key))
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Gets an item from the cache.
+
+        :param key: key to retrieve the item from
+        :return: the item if it is in the cache, None otherwise
+        """
+        if key is None:
+            return None
+        else:
+            if key in self.cache_data.keys():
+                # if the key is in the cache, move it to the top
+                item = self.cache_data.pop(key)
+                self.cache_data[key] = item
+                return item
+            else:
+                return None
